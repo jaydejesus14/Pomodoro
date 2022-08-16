326e8fd374c5df36cd56cd78fcb568001fcde2ea
ataraxielle <yoakenozero@gmail.com>
2022-08-16 17:17:58 +0800

    files organized

--- user/registration.php
+++ user/registration.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<link rel='stylesheet' href="style.css">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel='stylesheet' href="../style.css">
     
 </head>
 <body>
@@ -48,56 +45,9 @@
                                                 <li><a href="#">JavaScript</a></li>
                                             </ul>
                                             </div>
-</div>
-</nav>
-    <section class ="login">
-    <div class="container px-5 py-5 mx-auto">
-    <div class="card card0">
-        <div class="d-flex flex-lg-row flex-column-reverse">
-            <div class="card card1">
-                <div class="row justify-content-center my-auto">
-                    <div class="col-md-10 col-10 my-5">
-                        <div class="row justify-content-center px-3 mb-3">
-                            <img id = "logo" src="images/logo.png">
-                        </div>
-                        <h3 class="mb-5 text-center heading">WELCOME BACK!</h3>
-                        <hr class="solid">
+    </nav>
 
-                        <div class="form-group">
-                            <label class="form-control-label text-muted">Username</label>
-                            <input type="text" id="email" name="email" placeholder="Phone no or email id" class="form-control">
-                        </div>
 
-                        <div class="form-group">
-                            <label class="form-control-label text-muted">Password</label>
-                            <input type="password" id="psw" name="psw" placeholder="Password" class="form-control">
-                        </div>
-
-                        <div class="row justify-content-center my-3 px-3">
-                            <button class="btn-block btn-color" id = "login_btn">Login</button>
-                        </div>
-
-                       
-
-                        <div class="row justify-content-center my-2">
-                            <a href="#"><small class="text-muted">Forgot Password?</small></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="bottom text-center mb-5">
-                    <p href="#" class="sm-text mx-auto mb-3">Don't have an account?<button class="btn btn-white ml-2">Create new</button></p>
-                </div>
-            </div>
-            <div class="card card2">
-                <div class="my-auto mx-md-5 px-md-5 right">
-                    <h3 class="text-white">TITLE</h3>
-                    <small class="text-white">desciption</small>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</section>
 <?php
 
 ECHO "Hello World!<br>";
