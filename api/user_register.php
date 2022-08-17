eff29abca07c30d42b1606f576f9bfddab52fe95
Barskie <33131023+Barskie@users.noreply.github.com>
2022-08-17 13:25:34 +0800

    Create user_register.php
    variables not yet changed

---
+++ api/user_register.php
@@ -0,0 +1,34 @@
+<?php
+	include 'database.php';
+	$first_name = $_POST['first_name'];
+	$last_name  = $_POST['last_name'];
+    $user_name  = $_POST['user_name'];
+	$contact    = $_POST['contact'];
+    $password   = hash('ripemd160', $_POST['password']);
+	$address    = $_POST['address'];
+    $email      = $_POST['email'];
+	$role		= $_POST['role'];
+
+
+	
+	$sql = "SELECT * FROM tbl_user WHERE user_email = '$email' OR user_name = '$user_name'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		echo json_encode(array("status"=>false));
+	}
+	else
+	{
+    	$sql = "INSERT INTO tbl_user (user_first_name,user_last_name,user_name,user_contact_number,user_password,user_address,user_email,user_credential) 
+		VALUES 
+    	('$first_name','$last_name','$user_name','$contact','$password','$address','$email','$role')";
+		if (mysqli_query($conn, $sql)) {
+			echo json_encode(array("status"=>true));
+		} 
+		else 
+		{
+			echo json_encode(array("status"=>false));
+		}
+	}
+	mysqli_close($conn);
+?>
+
