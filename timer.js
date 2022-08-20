a9f72775aad3fccb9f3be40b95ada4f33d940d85
Barskie <33131023+Barskie@users.noreply.github.com>
2022-08-20 18:59:34 +0800

    timer start stop hehe

--- timer.js
+++ timer.js
@@ -1,36 +1,104 @@
-var minutes = 25;
-var seconds = "00";
-
-function template() {
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-}
-function start() {
-    minutes = 24;
-    seconds = 59;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-    var minutes_interval = setInterval(minutesTimer, 60000);
-    var seconds_interval = setInterval(secondsTimer, 1000);
+const timer = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 15,
+    longBreakInterval: 4,
+  };
+  
+  let interval;
+  
+  const mainButton = document.getElementById('timerStart_btn');
+  mainButton.addEventListener('click', () => {
+    const { action } = mainButton.dataset;
+    if (action === 'START') {
+      startTimer();
+    } else {
+      stopTimer();
+    }
+  });
+  
+  const modeButtons = document.querySelector('#js-mode-buttons');
+  modeButtons.addEventListener('click', handleMode);
+  
+  function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+  
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+  
+    return {
+      total,
+      minutes,
+      seconds,
+    };
+  }
+  
+  function startTimer() {
+    let { total } = timer.remainingTime;
+    const endTime = Date.parse(new Date()) + total * 1000;
+  
+    mainButton.dataset.action = 'STOP';
+    mainButton.textContent = 'STOP';
+    mainButton.classList.add('active');
+  
+    interval = setInterval(function() {
+      timer.remainingTime = getRemainingTime(endTime);
+      updateClock();
+  
+      total = timer.remainingTime.total;
+      if (total <= 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+  
+  function stopTimer() {
+    clearInterval(interval);
+  
+    mainButton.dataset.action = 'START';
+    mainButton.textContent = 'START';
+    mainButton.classList.remove('active');
+  }
+  
+  function updateClock() {
+    const { remainingTime } = timer;
+    const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+    const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+  
+    const min = document.getElementById('js-minutes');
+    const sec = document.getElementById('js-seconds');
+    min.textContent = minutes;
+    sec.textContent = seconds;
+  }
+  
+  function switchMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+      total: timer[mode] * 60,
+      minutes: timer[mode],
+      seconds: 0,
+    };
+  
+    document
+      .querySelectorAll('button[data-mode]')
+      .forEach(e => e.classList.remove('active'));
+    document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
     
-    function minutesTimer(){
-        minutes = minutes - 1;
-        document.getElementById("minutes").innerHTML = minutes;
-
-    }
-    function secondsTimer(){
-        seconds = seconds - 1
-        document.getElementById("seconds").innerHTML = seconds;
-        if(seconds <= 0){
-            if(minutes <= 0){
-                clearInterval(minutes_interval);
-                clearInterval(seconds_interval);
-
-            }
-            seconds = 60;
-        }
-    }
-
-}
+  
+    updateClock();
+  }
+  
+  function handleMode(event) {
+    const { mode } = event.target.dataset;
+  
+    if (!mode) return;
+  
+    switchMode(mode);
+    stopTimer();
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    switchMode('pomodoro');
+  });
