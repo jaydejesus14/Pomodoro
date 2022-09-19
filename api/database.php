823a2620e4f6dd84ae5b9d7dfcb044d79758e5d2
unknown <hendrixnagle@yahoo.com>
2022-09-20 00:54:58 +0800

    plugins

--- api/database.php
+++ api/database.php
@@ -1,7 +1,13 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$db="pomodoro";
-	$conn = mysqli_connect($servername, $username, $password,$db);
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $db="pomodoro";
+	// $conn = mysqli_connect($servername, $username, $password,$db);
+require_once __DIR__ .'/vendor/autoload.php';
+$con = new MongoDB\Client("mongodb://localhost:27017");
+
+$db = $con->pomodoro;
+
+
 ?>
--- api/user_register.php
+++ api/user_register.php
@@ -7,27 +7,33 @@
     $email      = $_POST['email'];
 // need to change id (eto ung nasa post)
 
+	// if($db->users->insertOne(["Fname" => $first_name])){
+	// 	echo 'success';
+	// }else{
+	// 	echo 'mali';
+	// }
+		echo json_encode($_POST);
 	
-	$sql = "SELECT * FROM user_info WHERE user_email = '$email' OR user_uname = '$user_name'";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-		echo json_encode(array("status"=>false));
-	}
-	else
-	{
-        //change column names 
-    	$sql = "INSERT INTO user_info (user_fname,user_lname,user_uname,user_email,user_password) 
-		VALUES 
-    	('$first_name','$last_name','$user_name','$email','$password')";
-		if (mysqli_query($conn, $sql)) {
-			echo json_encode(array("status"=>true));
-		} 
-		else 
-		{
-			echo json_encode(array("status"=>false));
-		}
-	}
-	mysqli_close($conn);	
+	// $sql = "SELECT * FROM user_info WHERE user_email = '$email' OR user_uname = '$user_name'";
+	// $result = $conn->query($sql);
+	// if ($result->num_rows > 0) {
+	// 	echo json_encode(array("status"=>false));
+	// }
+	// else
+	// {
+    //     //change column names 
+    // 	$sql = "INSERT INTO user_info (user_fname,user_lname,user_uname,user_email,user_password) 
+	// 	VALUES 
+    // 	('$first_name','$last_name','$user_name','$email','$password')";
+	// 	if (mysqli_query($conn, $sql)) {
+	// 		echo json_encode(array("status"=>true));
+	// 	} 
+	// 	else 
+	// 	{
+	// 		echo json_encode(array("status"=>false));
+	// 	}
+	// }
+	// mysqli_close($conn);	
 
 	/*$response = array{
 		'status' => 0,
