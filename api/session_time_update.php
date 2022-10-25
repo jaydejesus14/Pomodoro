610352e468f030a09af584b2ba96491225bf18d7
unknown <hendrixnagle@yahoo.com>
2022-10-25 17:04:58 +0800

    update pomodoro timer

---
+++ api/session_time_update.php
@@ -0,0 +1,25 @@
+<?php
+include 'database.php';
+$id    = $_POST['id'];
+$pomodoro  = $_POST['pomodoro'];
+$short_break = $_POST['short_break'];
+$long_break = $_POST['long_break'];
+
+
+
+$json_return = array();
+
+$is_insert = $db->groupTask->updateOne(
+        [ '_id' =>  new MongoDB\BSON\ObjectId ($id)],
+        [ '$set' => [ 'pomodoro' => $pomodoro, 'short_break' => $short_break, 'long_break' => $long_break ]]
+    );
+if($is_insert){
+    $json_return['status'] = 'success';
+}else{
+    $json_return['status'] = 'failed';
+}
+
+echo json_encode($json_return);
+
+
+?>
