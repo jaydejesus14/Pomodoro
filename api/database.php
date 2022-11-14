ca3a0e8f45f115d5f59f6f0fbc74d77e70d7701c
Barskie <33131023+Barskie@users.noreply.github.com>
2022-11-14 14:05:38 +0800

    TANGINA

--- api/database.php
+++ api/database.php
@@ -3,9 +3,8 @@
 
 require_once __DIR__ .'/vendor/autoload.php';
 
-$config_var = getenv('MONGODB_URI');
+$config_var = getenv('MONGODB_URL');
 $con = new MongoDB\Client($config_var);
 $db = $con->pomodoro;
 
-echo $config_var;
 ?>
