2b3019cce2fb96c40f51d2f55ba7682d03c5f297
unknown <hendrixnagle@yahoo.com>
2022-11-17 03:08:57 +0800

    Update reset_task_session.php

--- api/reset_task_session.php
+++ api/reset_task_session.php
@@ -2,10 +2,31 @@
 	include 'database.php';
     $id = $_POST['id'];
 
-    $is_insert = $db->sessionTask->updateMany(
+    $is_update = $db->sessionTask->updateMany(
         [ 'routineId' =>  $id],
         [ '$set' => [ 'is_complete' => false ]]
     );
+
+
+    $where = array(
+        '_id' => new MongoDB\BSON\ObjectId ($id)
+    );
+    
+    $cursor = $db->session->find($where);
+    $docs = $cursor->toArray();
+    
+    // if($_POST['key'] == 'end'){
+    $total = 1;
+    if(isset($docs[0]['times_done'])){
+        $total = $docs[0]['times_done'] + 1;
+    }
+        $for_update_section['times_done'] = $total;
+        // $for_update_section['end_date'] = $_POST['date'];
+    
+        $is_insert = $db->session->updateOne(
+            [ '_id' =>  new MongoDB\BSON\ObjectId ($id)],
+            [ '$set' => $for_update_section]
+        );
     
     if($is_insert){
         echo 'true';
