6874c459fbc0f5cd490c2f78d2bc979058932ed5
unknown <hendrixnagle@yahoo.com>
2022-11-16 21:07:17 +0800

    up

--- api/reports.php
+++ api/reports.php
@@ -1,60 +1,70 @@
 <?php
 	include 'database.php';
 
-    
-$where = array(
-    'user_id' => $_POST['user_id']
-);
+$user_id = $_POST['user_id'];
 
-$select_fields = array(
-    'routine_name' => 1,
-    'pomodoro' => 1,
-    'short_break' => 1,
-    'long_break' => 1
-);
 
-$options = array(
-    'projection' => $select_fields
-);
-
-$cursor = $db->session->find($where, $options);
-$docs = $cursor->toArray();
 $json_return = array();
-$json_array = array();
+$assessment_where_clause = array(
+    'user_id' => $user_id
+);
 
-foreach ($docs as $key => $value){
-    $subtask = array();
-    //for subtask
-    $where = array(
-        'routineId' => $value['_id']->__toString()
+$assessment_select_fields = array(
+    'points' => 1,
+    'date_created' => 1,
+    'time_created' => 1
+);
+
+$assessment_options = array(
+    'projection' => $assessment_select_fields
+);
+
+$cursor = $db->assessment->find($assessment_where_clause, $assessment_options);
+$assessments = $cursor->toArray();
+
+$json_return['assessments'] = $assessments;
+
+$todo_where_clause = array(
+    'user_id' => $user_id
+);
+
+$todo_select_fields = array(
+    'task_name' => 1,
+    'pomodoro' => 1,
+    'start_date' => 1,
+    'end_date' => 1,
+    'start_time' => 1,
+    'end_time' => 1
+);
+
+$todo_options = array(
+    'projection' => $todo_select_fields
+);
+
+$cursor = $db->groupTask->find($todo_where_clause, $todo_options);
+$todo = $cursor->toArray();
+$todoArray = array();
+foreach($todo as $key => $value){
+    $subtask_where_clause = array(
+        'majorTaskId' => $value['_id']->__toString()
     );
-    $select_fields = array(
-        // '_id' => 1,
-        'name' => 1,
-        'notes' => 1
-        
-    );
-    $options = array(
-        'projection' => $select_fields
-    );
-
-    $subtaskqry = $db->sessionTask->find($where, $options);
-    $return = $subtaskqry->toArray();
-
-    foreach ($return as $session_task){
-        $subtask[] = $session_task;
-    }
-
-    //end subtask
-    $json_array['id'] = $value['_id'];
-    $json_array['pomodoro'] = $value['pomodoro'];
-    $json_array['routine_name'] = $value['routine_name'];
-    $json_array['short_break'] = $value['short_break'];
-    $json_array['long_break'] = $value['long_break'];
-    $json_array['session_task'] = $subtask;
-    $json_return[] = $json_array;
     
+    $subtask_select_fields = array(
+        'subtaskName' => 1
+    );
+    
+    $subtask_options = array(
+        'projection' => $subtask_select_fields
+    );
+    
+    $cursor = $db->majorSubTask->find($subtask_where_clause, $subtask_options);
+    $subtask_no = $cursor->toArray();
+ $value['subtask_no'] = count($subtask_no);   
+
+ $todoArray[] = $value;
 }
+$json_return['task'] = $todoArray;
+
 echo json_encode($json_return);
 ?>
 
