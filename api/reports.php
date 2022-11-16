ae8c8fff058a293b0b1103d0b93d68311bf9b830
unknown <hendrixnagle@yahoo.com>
2022-11-17 02:49:15 +0800

    up

--- api/reports.php
+++ api/reports.php
@@ -67,6 +67,46 @@ foreach($todo as $key => $value){
 }
 $json_return['task'] = $todoArray;
 
+//routine
+$routineArray = array();
+$routine_where_clause = array(
+    'user_id' => $user_id
+);
+
+$routine_select_fields = array(
+    'routine_name' => 1,
+    'pomodoro' => 1,
+);
+
+$routine_options = array(
+    'projection' => $routine_select_fields
+);
+
+$cursor = $db->session->find($routine_where_clause, $routine_select_fields);
+$routine = $cursor->toArray();
+
+foreach($routine as $key => $value){
+    // if(isset($value['end_time'])){
+        $subtask_where_clause = array(
+            'routineId' => $value['_id']->__toString()
+        );
+        
+        $subtask_select_fields = array(
+            'task_name' => 1
+        );
+        
+        $subtask_options = array(
+            'projection' => $subtask_select_fields
+        );
+        
+        $cursor = $db->sessionTask->find($subtask_where_clause, $subtask_options);
+        $subtask_no = $cursor->toArray();
+        $value['subtask_no'] = count($subtask_no);   
+
+        $routineArray[] = $value;
+    }
+// }
+$json_return['routine'] = $routineArray;
 echo json_encode($json_return);
 ?>
 
