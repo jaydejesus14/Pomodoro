ab2ab13d5b1156f57233d346f39312355c08236a
Barskie <33131023+Barskie@users.noreply.github.com>
2022-11-18 11:47:38 +0800

    Updated Calendar
    CSS Broken

--- api/get_date.php
+++ api/get_date.php
@@ -3,11 +3,10 @@
 include 'database.php';
 
 $cursor = $db -> groupTask -> find();
-
+$route = $db -> session -> find();
 $date = $cursor -> toArray();
+$routine = $route -> toArray();
 
-echo json_encode($date);
-
-
+echo json_encode(array_merge($date,$routine));
 
 ?>
