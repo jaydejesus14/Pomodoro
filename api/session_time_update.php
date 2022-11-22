38c704b3c74cf4087a551bc7d10acb709269b2c5
ataraxielle <yoakenozero@gmail.com>
2022-11-22 14:23:07 +0800

    routine completed update and customize timer

--- api/session_time_update.php
+++ api/session_time_update.php
@@ -9,16 +9,30 @@ $long_break = $_POST['long_break'];
 
 $json_return = array();
 
-$is_insert = $db->groupTask->updateOne(
+if($_POST['key'] == 'routine'){
+    $is_insert = $db->session->updateOne(
         [ '_id' =>  new MongoDB\BSON\ObjectId ($id)],
         [ '$set' => [ 'pomodoro' => $pomodoro, 'short_break' => $short_break, 'long_break' => $long_break ]]
     );
-if($is_insert){
-    $json_return['status'] = 'success';
-}else{
-    $json_return['status'] = 'failed';
-}
+    if($is_insert){
+        $json_return['status'] = 'success';
+    }else{
+        $json_return['status'] = 'failed';
+    }
 
+}
+else{
+
+    $is_insert = $db->groupTask->updateOne(
+            [ '_id' =>  new MongoDB\BSON\ObjectId ($id)],
+            [ '$set' => [ 'pomodoro' => $pomodoro, 'short_break' => $short_break, 'long_break' => $long_break ]]
+        );
+    if($is_insert){
+        $json_return['status'] = 'success';
+    }else{
+        $json_return['status'] = 'failed';
+    }
+}
 echo json_encode($json_return);
 
 
