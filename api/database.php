d7933cf1213163bc36f8fd2363f14ca557063416
Barskie <33131023+Barskie@users.noreply.github.com>
2022-11-24 17:16:13 +0800

    for heroku link

--- api/database.php
+++ api/database.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
-$con = new MongoDB\Client("mongodb+srv://admin:bardug123@pomodoro.ixsez7h.mongodb.net/?retryWrites=true&w=majority");
 
+$config_var = getenv('MONGODB_URL');
+$con = new MongoDB\Client($config_var);
 $db = $con->pomodoro;
 
 
