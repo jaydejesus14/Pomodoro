937cb96054472a8c30ae183e5602cdfbe8090ff9
Barskie <33131023+Barskie@users.noreply.github.com>
2022-11-24 16:16:31 +0800

    oops

--- api/database.php
+++ api/database.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
-$con = new MongoDB\Client("mongodb+srv://adminbardug123@pomodoro.ixsez7h.mongodb.net/?retryWrites=true&w=majority");
+$con = new MongoDB\Client("mongodb+srv://admin:bardug123@pomodoro.ixsez7h.mongodb.net/?retryWrites=true&w=majority");
 
 $db = $con->pomodoro;
 
