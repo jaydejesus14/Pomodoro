c9ac5239584db978d9ef16567621acc001aa131c
Barskie <33131023+Barskie@users.noreply.github.com>
2022-11-24 16:15:53 +0800

    database link

--- api/database.php
+++ api/database.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
-$con = new MongoDB\Client("mongodb://localhost:27017");
+$con = new MongoDB\Client("mongodb+srv://adminbardug123@pomodoro.ixsez7h.mongodb.net/?retryWrites=true&w=majority");
 
 $db = $con->pomodoro;
 
