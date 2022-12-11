a88881462b5ebf7f1fa28e5e5949ad8ae46c725e
Barskie <33131023+Barskie@users.noreply.github.com>
2022-12-11 20:52:00 +0800

    test

--- api/database.php
+++ api/database.php
@@ -1,9 +1,11 @@
 <?php
 require_once __DIR__ .'/vendor/autoload.php';
 
-$config_var = getenv('MONGODB_URL');
-$con = new MongoDB\Client($config_var);
+// $config_var = getenv('MONGODB_URL');
+// $con = new MongoDB\Client($config_var);
+// $db = $con->pomodoro;
+
+$con = new MongoDB\Client("mongodb+srv://admin:bardug123@pomodoro.ixsez7h.mongodb.net/?retryWrites=true&w=majority");
+
 $db = $con->pomodoro;
-
-
 ?>
